464352e773496a3c32adb28b8bb754f65fdeb6c7
SerhiyGuivan <serhiy.guivan@gmail.com>
2016-01-16 16:17:04 +0300

    services

--- js/main.js
+++ js/main.js
@@ -8,5 +8,23 @@
 		    var id = $(this).attr('id');
 			$('html, body').animate({scrollTop: $('#'+id).offset().top}, 800);
 		});
+
+		var jcarousel = $('.bk-services-1 .jcarousel');
+		if(jcarousel.length){
+	        jcarousel
+	            .jcarousel({
+	                wrap: 'circular'
+	            });
+		}
+
+        $('.jcarousel-control-prev')
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .jcarouselControl({
+                target: '+=1'
+            });
 	});
 })(jQuery);
